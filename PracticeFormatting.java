f6fe1fb4e65f11c9d83267bd7b74f4a4b889e16b
bao-bmcc <55031239+bao-bmcc@users.noreply.github.com>
2019-09-14 12:20:24 -0400

    Add files via upload

---
+++ PracticeFormatting.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guangjian.bao
+ */
+public class PracticeFormatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("\t\tO'Malley say, \"practice\\makes perfet!\"\n");
+        System.out.println("\\\\\\\\\\\\\\\\\\");
+        System.out.println("@");
+        System.out.println("@\t\t r-p-o-h-e-s-s-a-g-r");
+        System.out.println("@\n@\n@\n");
+        System.out.println("@\t\t\tr-p-o-h-e-s-s-a-g-r");
+        System.out.println("\t\twho");
+        System.out.println("@ a) , b;!");
+        System.out.println("@ upnowgath");
+        System.out.println("\t\t\t PPEGORHRASS");
+        System.out.println("\t\t\t\teringint(0-");
+        System.out.println("@ aThe:1)");
+        System.out.println("@\t\t\teA");
+        System.out.println("@\t\t\t\t!P:");
+        System.out.println("@s\t\t\t\t\ta");    
+        System.out.print("@\t\t\t\t(r\n");
+        System.out.println("@rIvinG\t\t\t\t.gRaEaPsPhos\n");
+        System.out.println("@\t\t\t\t\t\tto"); 
+        System.out.println("@ rea(be)rran(com)gi(e)ngly");
+        System.out.println("@ ,grasshopper;");
+        System.out.println("@");
+        System.out.println("\\\\\\\\\\\\\\\\");
+    }
+}
