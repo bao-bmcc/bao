ea865eb76c7739f768c2cf58be1b45a067b6d537
bao-bmcc <55031239+bao-bmcc@users.noreply.github.com>
2019-09-14 11:05:20 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guangjian.bao
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.print("hello world");
+       System.out.print("\n\n\"dead\",oh no!, \\\\"
+               + "good");
+       System.out.println("\tbe care key worlds");
+       System.out.print("now,\\\\\\we are fine without anxiet");
+       System.out.print("now,\\\\\\we are fine without anxiet");
+       System.out.print("\n\tThis is a test for mysef;");
+    }
+}
